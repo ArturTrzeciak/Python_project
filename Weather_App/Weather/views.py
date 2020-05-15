06500ba86e3f16c880b33a7ebd47a1d021b7b2d9
artur_trzeciak <artur.alfa.gg@gmail.com>
2020-05-15 08:38:37 +0200

    Adding the ability to add new cities to the database
    Dodanie możliwości dodawania nowych miast do bazy danych, dodanie 3 podstawowych miast przez admina, korekta błędów.

--- Weather_App/Weather/views.py
+++ Weather_App/Weather/views.py
@@ -1,19 +1,32 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
 
 def index(request):
     appid = 'bc084494caa58d430710c47c1d6397a0'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
 
-    city = 'London'
-    res = requests.get(url.format(city)).json()
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
 
-    city_info = {
-        'city': city,
-        'temp': res["main"]["temp"],
-        'icon': res["weather"][0]["icon"]
-    }
+    form = CityForm()
 
-    conplex = {'info': city_info}
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        сity_info = {
+           'city': city.name,
+           'temp': res["main"]["temp"],
+           'icon': res["weather"][0]["icon"]
+        }
+
+        all_cities.append(сity_info)
+
+    conplex = {'all_info': all_cities, 'form': form}
 
     return render(request, 'Wether/index.html', conplex)
